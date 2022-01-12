d79c2d17056031d98a764e718bfc229db5b6ecf4
Choi, Hwa-Lang <hl8469@gmail.com>
2022-01-12 12:14:38 +0900

    Update postgres_docker.sh

--- fastapi/postgres_docker.sh
+++ fastapi/postgres_docker.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run -d --name postgresql -p 5432:5432 -e POSTGRES_PASSWORD=postgres -v $HOME/pg_data:/var/lib/postgresql/data postgres:13.4
+sudo docker run -d --name postgresql -p 5432:5432 -e POSTGRES_PASSWORD=postgres -v $HOME/pg_data:/var/lib/postgresql/data postgres:13.4
